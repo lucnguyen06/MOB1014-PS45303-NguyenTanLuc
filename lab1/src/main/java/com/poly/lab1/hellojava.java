21d2fe43db767320a1c5f2bf10500d96cb59b354
Lực Nguyễn - RIO <lucnguyen0562@gmail.com>
2026-09-17 12:26:33 +0700

    donelab1

---
+++ lab1/src/main/java/com/poly/lab1/hellojava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.lab1;
+
+/**
+ *
+ * @author Admin
+ */
+public class hellojava {
+    public static void main(String[] args) {
+        System.out.println("Ho va ten: Nguyen Tan Luc \n MSSV: PS45303");
+    }
+    
+}
